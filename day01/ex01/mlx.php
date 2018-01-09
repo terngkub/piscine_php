898c405ce4ef38f1497c74f936c21de0a047bb28
terngkub <terngnattapol@gmail.com>
2018-01-09 22:04:51 +0100

    finish ex00, ex01, ex02

---
+++ day01/ex01/mlx.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+
+for ($i = 0; $i < 10; $i++)
+{
+	for ($j = 0; $j < 100; $j++)
+	{
+		echo "X";
+	}
+	echo "\n";
+}
+
+?>
---
+++ day01/ex02/oddeven.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+
+echo "Enter a number: ";
+while ($number = fgets(STDIN))
+{
+	$number = trim($number);
+	if (is_numeric($number) && is_int($number + 0))
+	{
+		$number = $number + 0;
+		if ($number % 2 == 0)
+		{
+			echo "The number $number is even\n";
+		}
+		else
+		{
+			echo "The number $number is odd\n";
+		}
+	}
+	else
+	{
+		echo "'$number' is not a number\n";
+	}
+	echo "Enter a number: ";
+}
+
+?>
